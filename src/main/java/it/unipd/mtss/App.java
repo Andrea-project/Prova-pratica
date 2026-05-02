9cf24ef67f629474ab25aa7639139fc214f8a9e4
matteo <matteo.cuogo.1@studenti.unipd.it>
2026-05-02 12:19:55 +0200

    configurazione file maven

---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////
+// Matteo Cuogo 2111013
+// Andrea Scantamburlo 2113201
+// //////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        System.out.println( "Hello World!" );
+    }
+}
